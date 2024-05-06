53a01798eba203197594ca4c8ffbfe4f3d19dca2
JeffeVargasP <jeffersonvargas745@gmail.com>
2024-05-06 01:30:09 -0300

    feat: added footer and what style is being used

--- script.js
+++ script.js
@@ -1,60 +1,80 @@
-// Selecionando os elementos do DOM
-const box = document.querySelector('.box');
-const div = document.querySelector('.div');
-const buttons = document.querySelectorAll('li');
-const option = document.querySelector('.option');
+const box = document.querySelector(".box");
+const div = document.querySelector(".div");
+const buttons = document.querySelectorAll("li");
+const option = document.querySelector(".option");
+const justifyContentText = document.querySelector(".justify-content-text");
+const alignItemsText = document.querySelector(".align-items-text");
 
-// Função para atualizar o texto da div e aplicar estilos ao box
-function updateBox(button) {
-    // Removendo o conteúdo anterior da div
-    if (div.hasChildNodes()) {
-        div.removeChild(div.firstChild);
-    }
-    // Adicionando o texto do botão à div
-    div.appendChild(document.createTextNode(button.textContent));
-    // Exibindo a caixa
-    box.style.display = 'flex';
+function updateJustifyContentText(text) {
+  justifyContentText.textContent = `justify-content: ${text.replace('Align', '').toLowerCase()};`;
 }
 
-// Função para aplicar estilos de justifyContent ao box
+function updateAlignItemsText(text) {
+  alignItemsText.textContent = `align-items: ${text.replace('Align', '').toLowerCase()};`;
+}
+
+function updateButtonClassForJustifyContent(button) {
+  const parent = button.parentElement.parentElement;
+  const children = parent.querySelectorAll("li");
+  children.forEach((child) => {
+    child.classList.remove("active");
+  });
+  button.classList.add("active");
+}
+
+function updateButtonClassForAlignItems(button) {
+  const parent = button.parentElement.parentElement;
+  const children = parent.querySelectorAll("li");
+  children.forEach((child) => {
+    child.classList.remove("active");
+  });
+  button.classList.add("active");
+}
+
 function applyJustifyContentStyle(buttonValue) {
-    if (buttonValue === 'Align Left') {
-        box.style.justifyContent = 'flex-start';
-    } else if (buttonValue === 'Align Center') {
-        box.style.justifyContent = 'center';
-    } else if (buttonValue === 'Align Right') {
-        box.style.justifyContent = 'flex-end';
-    } else {
-        box.style.justifyContent = 'space-between';
-    }
+  if (buttonValue === "Align Left") {
+    box.style.justifyContent = "flex-start";
+  } else if (buttonValue === "Align Center") {
+    box.style.justifyContent = "center";
+  } else if (buttonValue === "Align Right") {
+    box.style.justifyContent = "flex-end";
+  } else {
+    box.style.justifyContent = "space-between";
+  }
 }
 
-// Função para aplicar estilos de alignItems ao box
 function applyAlignItemsStyle(buttonValue) {
-    if (buttonValue === 'Align Top') {
-        box.style.alignItems = 'flex-start';
-    } else if (buttonValue === 'Align Center') {
-        box.style.alignItems = 'center';
-    } else {
-        box.style.alignItems = 'flex-end';
-    }
+  if (buttonValue === "Align Top") {
+    box.style.alignItems = "flex-start";
+  } else if (buttonValue === "Align Center") {
+    box.style.alignItems = "center";
+  } else {
+    box.style.alignItems = "flex-end";
+  }
 }
 
-// Iterando sobre os botões e adicionando eventos de clique
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        // Atualizando o texto da div
-        updateBox(button);
-        // Obtendo o texto do botão pai do botão clicado
-        const buttonValue = button.textContent;
-        // Verificando se a opção é para justificar conteúdo ou alinhar itens
-        const optionType = button.parentElement.parentElement.querySelector('h2').textContent.replace(/\s/g, '');
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    box.style.display = "flex";
+    button.parentElement.parentElement.querySelector("h2").textContent ===
+    "Justify Content"
+      ? updateButtonClassForJustifyContent(button)
+      : updateButtonClassForAlignItems(button);
+    const buttonValue = button.textContent;
 
-        // Aplicando estilos dependendo do tipo de opção selecionada
-        if (optionType === 'JustifyContent') {
-            applyJustifyContentStyle(buttonValue);
-        } else {
-            applyAlignItemsStyle(buttonValue);
-        }
-    });
+    button.parentElement.parentElement.querySelector("h2").textContent ===
+    "Justify Content"
+      ? updateJustifyContentText(buttonValue)
+      : updateAlignItemsText(buttonValue);
+
+    const optionType = button.parentElement.parentElement
+      .querySelector("h2")
+      .textContent.replace(/\s/g, "");
+
+    if (optionType === "JustifyContent") {
+      applyJustifyContentStyle(buttonValue);
+    } else {
+      applyAlignItemsStyle(buttonValue);
+    }
+  });
 });
